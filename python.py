610e7ebaac65e076943efc31c63252f5579f92c5
ckc0774 <153164905+ckc0774@users.noreply.github.com>
2024-11-03 08:20:35 +0000

    max number

--- python.py
+++ python.py
@@ -1,6 +1,10 @@
-def sum_n(n):
-    s = 0
-    for i in range(1,n+1):
-        s=s+i*i
-    return s
-print(sum_n(10))
+def find_max(a):
+    n = len(a)
+    max_v = a[0]
+    for i in range(1,n):
+        if a[i] > max_v:
+            max_v=a[i]
+    return max_v
+
+v=[17,9,18,33,58,7,33,42]
+print(find_max(v))
